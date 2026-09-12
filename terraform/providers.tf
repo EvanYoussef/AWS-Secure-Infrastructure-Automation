448ca92174454e9529fe899ebf024bcfbfa25a07
evanyoussef <evanyoussef@yahoo.com>
2026-09-11 22:36:19 -0400

    Updated providers.tf to access terraform state Backend

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,4 +1,12 @@
 terraform {
+  backend "s3" {
+    bucket       = "evanyoussef-devsecops-terraform-state-1366463268"
+    key          = "devsecops/terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
+  }
+
   required_providers {
     aws = {
       source = "hashicorp/aws"
